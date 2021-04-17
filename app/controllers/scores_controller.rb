1e39e19ffa1c7b7f5af092a52e2e0bb6cc5f2dd1
Herbert Ilhan Tanujaya <mail@herbert.id>
2021-04-17 10:23:45 +0800

    Use ag grid

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,13 +2,6 @@
 
 class ScoresController < ApplicationController
   def list
-    @scores = Score.all.order(score: :desc).includes(:chart, chart: [:song])
-    @scores = @scores.where('charts.stars' => params['stars']) unless params['stars'].nil?
-    case params['played']
-    when 'true'
-      @scores = @scores.where.not('scores.best_score' => nil)
-    when 'false'
-      @scores = @scores.where('scores.best_score' => nil)
-    end
+    @scores = Score.all.includes(:chart, chart: [:song])
   end
 end
